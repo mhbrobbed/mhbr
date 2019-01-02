9d817b580118db69e7ef13490407f3e89b440087
mhbrobbed <46312472+mhbrobbed@users.noreply.github.com>
2019-01-02 19:04:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+var characters = {'Captain America':["r","S","sh","s", "n", "2"],
+                 'Iron Man': ["s","B", "g", "r", "n", "1"], 
+                 'Dr. Strange':["r","B", "sh", "a", "n", "3"], 
+                 'Hulk':["s","S","s", "s", "n", "1"], 
+                 'Falcon':["r","S","g", "a", "n", "1"], 
+                 'Black Panther':["r","Bth","sh", "r", "n", "1"], 
+                 'Spider Man':["s","Bth", "g", "a", "n", "1"], 
+                 'Winter Soldier':["s","S", "g", "s", "n", "2"],
+                 'Thor':["s", "S", "s", "s", "n", "2"],
+             	 'Star Lord': ["r", "S", "g", "s", "s", "2"],
+             	 'Rocket': ["s", "S", "g", "r", "s", "2"],
+             	 'Black Widow': ["r", "Bth", "g", "a", "s", "2"],
+                 'Vision': ["r", "Bth", "g", "s", "s", "3"],
+                 'Scarlet Witch': ["r", "Bth", "s", "s", "s", "3"],
+     		     'Groot': ["r", "S", "s", "s", "s", "2"],
+ 				 'Gamora': ["r", "S", "s", "a", "s", "2"]}
+var count = {'Captain America':0,
+                 'Iron Man': 0, 
+                 'Dr. Strange':0, 
+                 'Hulk':0, 
+                 'Falcon':0, 
+                 'Black Panther':0, 
+                 'Spider Man':0, 
+                 'Winter Soldier':0,
+                 'Thor':0,
+             	 'Star Lord': 0,
+             	 'Rocket':0,
+             	 'Black Widow':0,
+                 'Vision': 0,
+                 'Scarlet Witch': 0,
+     		     'Groot': 0,
+ 				 'Gamora': 0}
+var heroes = ['Captain America','Iron Man','Dr. Strange', 'Hulk', 'Falcon', 'Black Panther', 'Spider Man', 'Winter Soldier','Thor','Star Lord','Rocket','Black Widow','Vision','Scarlet Witch','Groot','Gamora']
+
+function pressedButton(){
+    
+    return "None"
+}
